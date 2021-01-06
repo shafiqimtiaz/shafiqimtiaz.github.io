f87c90a5bd350e851afdeba8cc9a5d76626d693e
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2021-01-06 23:50:52 +0600

    updated script

--- Ex3 Number Game/js/script.js
+++ Ex3 Number Game/js/script.js
@@ -1,25 +1,41 @@
+let low = 1;
+let high = 10;
+
 // random value generated 
-var y = Math.floor(Math.random() * 10 + 1);
+function getRandom(low, high) {
+    min = Math.ceil(low);
+    max = Math.floor(high);
 
-// counting the number of guesses 
-// made for correct Guess 
-var guess = 1;
+    //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+let randValue = getRandom(low, high);
+let guess = 1;
+
+// counting the number of guesses made for correct Guess 
 document.getElementById("submitguess").onclick = function () {
-
-    // number guessed by user      
-    var x = document.getElementById("guessField").value;
-
-    if (x == y) {
-        alert("CONGRATULATIONS!!! ATTEMPT: " + guess);
+    // number guessed by user     
+    // let input = document.getElementById("guessField").value;
+    for (let i = 0; i <= 3; i++) {
+        if (i == 3) {
+            alert("YOU LOSE :(\nCorrect answer was " + randValue);
+            break;
+        } else {
+            let input = prompt("Enter Guess:");
+            if (input == randValue) {
+                alert("CONGRATULATIONS, YOU WIN !!!\nATTEMPT: " + guess);
+                break;
+            }
+            else if (input > randValue) {
+                guess++;
+                alert("Correct answer is smaller! " + randValue);
+            }
+            else {
+                guess++;
+                alert("Correct answer is greater! " + randValue);
+            }
+        }
     }
-    else if (x > y) /* if guessed number is greater 
-               than actual number*/ {
-        guess++;
-        alert("SORRY!! TRY A SMALLER NUMBER");
-    }
-    else {
-        guess++;
-        alert("SORRY!! TRY A GREATER NUMBER")
-    }
+    location.reload();
 }
